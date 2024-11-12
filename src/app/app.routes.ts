efb0c0a1c05263789d73ce85143312a3aa1a42f8
Cristian-2020 <cafm20082008@gmail.com>
2024-11-11 18:54:02 -0600

    add-error

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './views/home/home.component';
 import { ViewPeliculasComponent } from './views/peliculas/view-peliculas/view-peliculas.component';
 import { AddPeliculasComponent } from './views/peliculas/add-peliculas/add-peliculas.component';
 import { EditPeliculasComponent } from './views/peliculas/edit-peliculas/edit-peliculas.component';
+import { ErrorComponent } from './views/error/error.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
     {path: "view", component: ViewPeliculasComponent},
     {path: "add", component: AddPeliculasComponent},
-    {path: "edit/:id", component: EditPeliculasComponent}
+    {path: "edit/:id", component: EditPeliculasComponent},
+    {path: "**", component: ErrorComponent}
 ];
