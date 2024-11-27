0c710a4cb1814ef7d1010aa6170a3c4f4552a011
Cristian-2020 <cafm20082008@gmail.com>
2024-11-26 21:11:28 -0600

    add-login-view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './includes/sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,SidebarComponent],
+  imports: [RouterOutlet,CommonModule,SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'app-peliculas';
+
+  constructor(public router: Router){}
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { ViewPeliculasComponent } from './views/peliculas/view-peliculas/view-pe
 import { AddPeliculasComponent } from './views/peliculas/add-peliculas/add-peliculas.component';
 import { EditPeliculasComponent } from './views/peliculas/edit-peliculas/edit-peliculas.component';
 import { ErrorComponent } from './views/error/error.component';
+import { LoginComponent } from './views/login/login.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
+    {path: "login", component: LoginComponent},
     {path: "view", component: ViewPeliculasComponent},
     {path: "add", component: AddPeliculasComponent},
     {path: "edit/:id", component: EditPeliculasComponent},
